696176cd0cae3d8d505b25fa2a56455f39e30ad6
Theralun <32106185+Theralun@users.noreply.github.com>
2020-07-09 07:52:48 +0200

    Initial commit

---
+++ src/components/molecules/index.js
@@ -0,0 +1,14 @@
+//#region > Imports
+// Import all components to export them for easy access from parent components
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+//#endregion
+
+//#region > Exports
+export { Footer, Navbar };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2020 Werbeagentur Christian Aichner
+ */
